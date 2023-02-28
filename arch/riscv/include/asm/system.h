cad041943068e2332ea6481485b4a3a475db3921
Shivaraj199 <shivarajthyvsp@gmail.com>
2023-02-28 15:38:29 +0100

    uboot init

--- arch/riscv/include/asm/system.h
+++ arch/riscv/include/asm/system.h
@@ -16,7 +16,37 @@ struct event;
  *
  */
 
+<<<<<<< HEAD
 /* Hook to set up the CPU (called from SPL too) */
 int riscv_cpu_setup(void *ctx, struct event *event);
+=======
+static inline void local_irq_save(
+	unsigned long flags __attribute__((unused)))
+{
+	__asm__ __volatile__ ("" : : : "memory");
+}
+
+static inline void local_irq_enable(void)
+{
+	__asm__ __volatile__ ("" : : : "memory");
+}
+
+static inline void local_irq_disable(void)
+{
+	__asm__ __volatile__ ("" : : : "memory");
+}
+
+static inline void local_save_flags(
+	unsigned long flags __attribute__((unused)))
+{
+	__asm__ __volatile__ ("" : : : "memory");
+}
+
+static inline void local_irq_restore(
+	unsigned long flags __attribute__((unused)))
+{
+	__asm__ __volatile__ ("" : : : "memory");
+}
+>>>>>>> b356a897b11ef19dcbe7870530f23f3a978c1714
 
 #endif	/* __ASM_RISCV_SYSTEM_H */
